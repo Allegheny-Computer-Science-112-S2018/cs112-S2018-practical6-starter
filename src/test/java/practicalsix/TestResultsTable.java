9e61690cae6d813188390fad6cb8f75d8831cd86
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2018-03-09 08:41:07 -0500

    Fixing package details in the TestResultsTable.

--- src/test/java/practicalsix/TestResultsTable.java
+++ src/test/java/practicalsix/TestResultsTable.java
@@ -1,10 +1,10 @@
-package labseven;
+package practicalsix;
 
 import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
-import labseven.data.ResultsTable;
+import practicalsix.data.ResultsTable;
 import org.junit.Test;
 
 import java.util.Arrays;
