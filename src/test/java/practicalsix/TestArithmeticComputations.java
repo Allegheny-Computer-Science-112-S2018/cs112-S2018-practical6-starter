400b76d78c7923ee96623cfed15087eb6c68ca1a
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2018-03-09 08:42:18 -0500

    Updating the TestArithmeticComputations with the required TODO markers.

--- src/test/java/practicalsix/TestArithmeticComputations.java
+++ src/test/java/practicalsix/TestArithmeticComputations.java
@@ -1,10 +1,10 @@
-package labseven;
+package practicalsix;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
-import labseven.computation.IterativeFibonacciComputation;
-import labseven.computation.RecursiveFibonacciComputation;
+import practicalsix.computation.IterativeFibonacciComputation;
+import practicalsix.computation.RecursiveFibonacciComputation;
 import org.junit.Test;
 
 import java.util.Arrays;
@@ -12,50 +12,24 @@ import java.util.Arrays;
 /**
  * A JUnit test suite for the different ArithmeticComputation implementations.
  *
- * <p>Please refer to this web site for the computed values:
- * http://planetmath.org/listoffibonaccinumbers
- *
  * <p>Please note that the computational cost of a certain algorithm
- * prevents the efficient testing of input values. For instance, running
- * this test suite with a new test calling compute(32) could take a long time!
+ * may prevent the efficient testing of input values. Be careful!
  *
  * @author Gregory M. Kapfhammer
  */
 
 public class TestArithmeticComputations {
 
-  /** Create the two objects used in all of the test suites. */
-  RecursiveFibonacciComputation recursiveFibonacci = new RecursiveFibonacciComputation();
-  IterativeFibonacciComputation iterativeFibonacci = new IterativeFibonacciComputation();
+  /** TODO: Create the two objects used in all of the test suites. */
+
+  /** TODO: Add in multiple test cases for the Factorial computations. */
 
   @Test
-  public void testFibonacciZero() {
-    assertEquals(recursiveFibonacci.compute(0), 0);
-    assertEquals(iterativeFibonacci.compute(0), 0);
+  public void testIterativeFactorial() {
   }
 
   @Test
-  public void testFibonacciOne() {
-    assertEquals(recursiveFibonacci.compute(1), 1);
-    assertEquals(iterativeFibonacci.compute(1), 1);
-  }
-
-  @Test
-  public void testFibonacciTen() {
-    assertEquals(recursiveFibonacci.compute(10), 55);
-    assertEquals(iterativeFibonacci.compute(10), 55);
-  }
-
-  @Test
-  public void testFibonacciFifteen() {
-    assertEquals(recursiveFibonacci.compute(15), 610);
-    assertEquals(iterativeFibonacci.compute(15), 610);
-  }
-
-  @Test
-  public void testFibonacciTwenty() {
-    assertEquals(recursiveFibonacci.compute(20), 6765);
-    assertEquals(iterativeFibonacci.compute(20), 6765);
+  public void testRecursiveFactorial() {
   }
 
 }
